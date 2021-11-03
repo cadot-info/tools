069b0b3098a00d456a11daba66be14f87bcb5da7
michael <michael@cadot.info>
2021-11-03 08:05:11 +0000

    add composer.json

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # tools
-# tools
+
+Functions:
+
+- returnAllLinks for seeks links and tests (start page,level of subpage)
+- E for echo immediatly (message)
+
+##utilisation
+
+use by traits
+
+
+example
+```
+use App\Tests\Tools;
+
+class ClientTest extends PantherTestCase
+{
+   use Tools;
+```
---
+++ Tools.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Tests;
+
+
+trait Tools
+{
+
+    public function returnAllLinks($start, $descent = 0, $links = [])
+    {
+        $exlinks = $links;
+        $client = static::createClient();
+        $crawler = $client->request('GET', $start);
+        //on regarde les liens de la page
+        foreach ($crawler->filter('a[href]') as $link) { // on ne prend pas les liens sans href
+            /** @var DOMElement $link */
+            $url = $link->getAttribute('href');
+            // on ne prend pas les liens déjà pris, ni les mailto ni les url extérieures, ni les bigpictures
+            if (!in_array(explode(':', $url)[0], ['mailto', 'http', 'https']) && explode('.', $url)[0] != 'www' &&  !isset($exlinks[$url]) && !in_array('bigpicture', explode(' ', $link->getAttribute('class')))) {
+                if ($descent > -1) { // si on est dans une récursivité acceptée
+                    $links = $this->returnAllLinks($url, $descent - 1, $links);
+                } else {
+                    $links[$url] = trim(preg_replace('/\s+/', ' ', str_replace(array("\n", "\r", ""), '', $link->nodeValue)));
+                }
+            }
+        }
+        return $links;
+    }
+    public function E($texte)
+    {
+        echo "- " . ucfirst($texte) . "\n";
+        ob_flush();
+    }
+}
